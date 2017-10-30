64bea3ece7013505cba8b6e136a46db3b0394d06
Tomasz Jonak <jonak.t.o@gmail.com>
2017-10-30 23:53:28 +0100

    Implementacja protokolu po stronie serwera + wektory testowe

--- implementacja2/sync/server/ServerProtocolProcessor.py
+++ implementacja2/sync/server/ServerProtocolProcessor.py
@@ -1,5 +1,6 @@
 from ..utils import StreamTokenWriter, StreamTokenReader, PersistentQueue
 import pathlib as pl
+import datetime as dt
 
 
 class ServerProtocolProcessor(object):
@@ -11,7 +12,7 @@ class ServerProtocolProcessor(object):
     oraz obsluge bledow z nimi zwiazanych.
     """
     def __init__(self, reader: StreamTokenReader.StreamTokenReader, writer: StreamTokenWriter.StreamTokenWriter,
-                 disk_cache: PersistentQueue.Queue):
+                 disk_cache: PersistentQueue.Queue, storage_root: (str, pl.Path), **kwargs):
         if not isinstance(reader, StreamTokenReader.StreamTokenReader):
             raise RuntimeError('Unsupported reader type')
         if not isinstance(writer, StreamTokenWriter.StreamTokenWriter):
@@ -19,31 +20,49 @@ class ServerProtocolProcessor(object):
         if not isinstance(disk_cache, PersistentQueue.Queue):
             raise RuntimeError('Unsupported disk cache type')
 
+        # lokacja na dysku w ktorej skladowane beda pliki
+        # wszystkie sciezki przeslane przez klienta
+        if isinstance(storage_root, str):
+            self.storage_root = pl.Path(storage_root)
+        elif isinstance(storage_root, pl.Path):
+            self.storage_root = storage_root
+        else:
+            raise RuntimeError('Unsupported storage_root type ({})'.format(repr(storage_root)))
+
         self.reader = reader
         self.writer = writer
         self.cache = disk_cache
+
+        self.options = kwargs
+
         self._file_description = {
             'filename': None,
             'file_pathobj': None,
-            'size': None,
-            'fd': None
+            'size': None
         }
 
         self.operation = self._send_cached_filename
 
     def _send_cached_filename(self):
+        """
+        (Synchronizacja) przesyl informacji o ostatnim wygenerowanym przez labview pliku
+        ktory zostal poprawnie zapisany na serwerze.
+        """
         newest_file = self.cache.get(wait_for_value=False)
         self.writer.write_token(newest_file)
 
         self.operation = self._process_filename
 
     def _process_filename(self):
+        """
+        Pobiera ze streamu pierwszy token, sprawdza czy jest poprawna sciezka systemowa
+        """
         filename = self.reader.get_token().decode('utf8')
         if not filename:
             return
         path = pl.Path(filename)
         if any(len(part) > 255 for part in path.parts):
-            # TODO: log some message?
+            # TODO: log some message? skip to next token?
             return
 
         self._file_description['filename'] = filename
@@ -59,7 +78,68 @@ class ServerProtocolProcessor(object):
         self.operation = self._process_file_bytes
 
     def _process_file_bytes(self):
-        raise RuntimeError('implement that')
+        """
+        Sekwencja odpowiedzialna za faktyczne odebranie pliku
+        TODO: checksuma
+        """
+        path = self._file_description['file_pathobj']
+        path.parent.mkdir(exist_ok=True, parents=True)
+        chunk_size = self.options.get('chunk_size', 8192)
+
+        size = self._file_description['size']
+        full_chunks = size // chunk_size
+        last_chunk_size = size % chunk_size
+        with path.open('wb') as fd:
+            for _ in range(full_chunks):
+                chunk = self.reader.get_bytes(chunk_size)
+                fd.writeall(chunk)
+            fd.writeall(self.reader.get_bytes(last_chunk_size))
+
+        self.operation = self._announce_response
+
+    def _announce_response(self):
+        """
+        Potwierdzenie odebrania pliku przez serwer, domyka transakcje
+        """
+        path = self._file_description['filename']
+        self.writer.write_token(path)
+
+        self.operation = self._update_cache
+
+    def newer_file(self, left, right):
+        """
+        Czynnosc potrzebna klientowi w momencie wygenerowania plikow przez labview przed 'wstaniem'
+        sync clienta. Przekazuje informacje o najswiezszym odebranym pliku (co do daty wygenerowania przez labview)
+        :param left: str - sciezka do lewego pliku (pathlike)
+        :param right: str - sciezka do prawego pliku (pathlike)
+        :return: bool - czy lewy jest nowszy od prawego
+        """
+        time_formatstr = self.options.get('formatstr', 'M%y%m%d-%H%M%S')
+
+        left_date = dt.datetime.strptime(left, time_formatstr)
+        right_date = dt.datetime.strptime(right, time_formatstr)
+        return left_date > right_date
+
+    def _update_cache(self):
+        """
+        Sprawdza zawartosc dyskowego cache, podmienia wartosc jesli jest nowsza
+        """
+        last_element = self.cache.get(wait_for_value=False)
+        current_element = self._file_description['filename']
+
+        if not last_element:
+            self.cache.put(current_element)
+        else:
+            last_path = pl.Path(last_element)
+            curr_path = pl.Path(current_element)
+            if self.newer_file(curr_path.name, last_path.name):
+                self.cache.pop(last_element)
+                self.cache.put(current_element)
+
+        # For testing purposes, this should always have one entry
+        assert self.cache.len() == 1
+
+        self.operation = self._process_filename
 
     def run(self):
         while True:
---
+++ implementacja2/sync/server/tests/vectors/generate_vectors.py
@@ -0,0 +1,79 @@
+import datetime as dt
+
+tadeusz_path = 'pan-tadeusz-czyli-ostatni-zajazd-na-litwie.txt'
+tadeusz_test_size = 1000
+
+# 'M%y%m%d/M%y%m%d-%H%M%S'
+base_date = dt.datetime.strptime('M000101-000000', 'M%y%m%d-%H%M%S')
+def get_name(case, file):
+    return (base_date + dt.timedelta(days=case, seconds=file)).strftime('M%y%m%d/M%y%m%d-%H%M%S')
+
+# 'M%y%m%d/M%y%m%d-%H%M%S'
+case_0_file_1_name = get_name(0, 0)
+with open('one_file.vector', 'wb') as dest_fd:
+    dest_fd.write('{}\r\n'.format(case_0_file_1_name).encode('utf8'))
+    dest_fd.write('{}\r\n'.format(tadeusz_test_size).encode('utf8'))
+    with open(tadeusz_path, 'rb') as source_fd:
+        dest_fd.write(source_fd.read(tadeusz_test_size))
+    dest_fd.write('\r\n'.encode('utf8'))
+
+case_1_file_1_name = get_name(1, 0)
+case_1_file_2_name = get_name(1, 1)
+with open('two_equall_length.vector', 'wb') as dest_fd:
+    dest_fd.write('{}\r\n'.format(case_1_file_1_name).encode('utf8'))
+    dest_fd.write('{}\r\n'.format(tadeusz_test_size).encode('utf8'))
+    with open(tadeusz_path, 'rb') as source_fd:
+        dest_fd.write(source_fd.read(tadeusz_test_size))
+    dest_fd.write('\r\n'.encode('utf8'))
+    dest_fd.write('{}\r\n'.format(case_1_file_2_name).encode('utf8'))
+    dest_fd.write('{}\r\n'.format(tadeusz_test_size).encode('utf8'))
+    with open(tadeusz_path, 'rb') as source_fd:
+        dest_fd.write(source_fd.read(tadeusz_test_size))
+    dest_fd.write('\r\n'.encode('utf8'))
+
+case_2_file_1_name = get_name(2, 0)
+case_2_file_2_name = get_name(2, 1)
+with open('two_increasing_length.vector', 'wb') as dest_fd:
+    dest_fd.write('{}\r\n'.format(case_2_file_1_name).encode('utf8'))
+    dest_fd.write('{}\r\n'.format(tadeusz_test_size).encode('utf8'))
+    with open(tadeusz_path, 'rb') as source_fd:
+        dest_fd.write(source_fd.read(tadeusz_test_size))
+    dest_fd.write('\r\n'.encode('utf8'))
+
+    dest_fd.write('{}\r\n'.format(case_2_file_2_name).encode('utf8'))
+    dest_fd.write('{}\r\n'.format(tadeusz_test_size*2).encode('utf8'))
+    with open(tadeusz_path, 'rb') as source_fd:
+        dest_fd.write(source_fd.read(tadeusz_test_size*2))
+    dest_fd.write('\r\n'.encode('utf8'))
+
+case_3_file_1_name = get_name(3, 0)
+case_3_file_2_name = get_name(3, 1)
+with open('two_decreasing_length.vector', 'wb') as dest_fd:
+    dest_fd.write('{}\r\n'.format(tadeusz_path).encode('utf8'))
+    dest_fd.write('{}\r\n'.format(tadeusz_test_size * 2).encode('utf8'))
+    with open(tadeusz_path, 'rb') as source_fd:
+        dest_fd.write(source_fd.read(tadeusz_test_size * 2))
+    dest_fd.write('\r\n'.encode('utf8'))
+
+    dest_fd.write('{}\r\n'.format(tadeusz_path).encode('utf8'))
+    dest_fd.write('{}\r\n'.format(tadeusz_test_size).encode('utf8'))
+    with open(tadeusz_path, 'rb') as source_fd:
+        dest_fd.write(source_fd.read(tadeusz_test_size))
+    dest_fd.write('\r\n'.encode('utf8'))
+
+
+small_size = 100
+case_4_file_2_name = get_name(4, 1)
+case_4_file_1_name = get_name(4, 0)
+with open('two_no_cache_update.vector', 'wb') as dest_fd:
+    dest_fd.write('{}\r\n'.format(case_4_file_2_name).encode('utf8'))
+    dest_fd.write('{}\r\n'.format(small_size).encode('utf8'))
+    with open(tadeusz_path, 'rb') as source_fd:
+        dest_fd.write(source_fd.read(small_size))
+    dest_fd.write('\r\n'.encode('utf8'))
+
+    dest_fd.write('{}\r\n'.format(case_4_file_1_name).encode('utf8'))
+    dest_fd.write('{}\r\n'.format(small_size).encode('utf8'))
+    with open(tadeusz_path, 'rb') as source_fd:
+        dest_fd.write(source_fd.read(small_size))
+    dest_fd.write('\r\n'.encode('utf8'))
