a0bcc9271586464a98f7878303d7459bbfdb337f
Tomasz Jonak <jonak.t.o@gmail.com>
2017-12-20 16:33:17 +0100

    Notatki na temat kompresji

---
+++ implementacja2/test_compression.py
@@ -0,0 +1,22 @@
+# Schematy
+
+# csv
+#   falki
+#   cos jeszcze?
+
+# Video:
+#   ffmpeg z driverem x264
+#   Jakis inny
+
+# Bezstratne
+#   bzip2
+#   zlib
+#   xz
+#   lzma
+#   gzip
+
+# Zbior testowy
+# dane z jednego dnia
+
+
+# Presety ffmpega
