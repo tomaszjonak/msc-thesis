2e064b02610daab21866c1fe15b95d4014cb7b8c
Tomasz Jonak <tomasz.jonak@forcepoint.com>
2017-12-28 13:07:36 +0100

    Podstawka pod obsluge SIGINT - wylaczania programu bez sypania wyjatkami

--- implementacja2/client.py
+++ implementacja2/client.py
@@ -7,6 +7,7 @@ import pathlib
 import queue
 import argparse
 import logging
+import signal
 
 
 logger = logging.getLogger(__name__)
@@ -39,27 +40,41 @@ def main():
     storage_root.mkdir(exist_ok=True, parents=True)
 
     stage_queue_path = config['stage_queue_path']
+    # Using same sqlite connection from multiple threads fails miserably, thus 3 'views'
     stage_queue_view1 = que.SqliteQueue(stage_queue_path)
     stage_queue_view2 = que.SqliteQueue(stage_queue_path)
     stage_queue_view3 = que.SqliteQueue(stage_queue_path)
 
     cache = que.SqliteQueue(config['cache_path'])
 
+    services = []
+
+    # def sigint_handler(signum, frame):
+    #     # TODO add timeouts to config
+    #     # logger.info('SIGINT trapped, services may take some time to shut down (if too long send few more sigints)')
+    #     logger.info('Gracefull shutdown is not yet supported, send sigint few more times (ctrl+c)')
+    #     [service.stop() for service in services]
+    #
+    # signal.signal(signal.SIGINT, sigint_handler)
+
     sync_queue = queue.Queue()
     logger.info('Starting synchronization service')
     sync_service = sc.SynchronizationService(sync_queue=sync_queue, queue_view=stage_queue_view2,
                                              storage_root=storage_root)
+    services.append(sync_service)
 
     labview_address = (config['provider']['host'], int(config['provider']['port']))
     logger.info('Starting receiver service')
     receiver_service = receiver.ReceiverService(labview_address, stage_queue_view3, storage_root,
                                                 cache, sync_queue, extensions=config['provider']['extensions'],
                                                 separator=config['provider']['separator'])
+    services.append(receiver_service)
 
     server_address = (config['server']['host'], int(config['server']['port']))
     logger.info('Starting sender service')
     sender_service = sender.SenderService(address=server_address, storage_root=storage_root,
                                           stage_queue=stage_queue_view1, sync_queue=None)
+    services.append(sender_service)
 
 
 main()
