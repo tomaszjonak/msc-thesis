272ad4decf427ce5342a8b99833f6f7bdb09bc45
Tomasz Jonak <tomasz.jonak@forcepoint.com>
2017-12-28 12:46:38 +0100

    Przygotowanie konfiguracji klienta i serwera

--- implementacja2/client.py
+++ implementacja2/client.py
@@ -2,23 +2,64 @@ import sync.client.ReceiverService as receiver
 import sync.client.SenderService as sender
 import sync.utils.PersistentQueue as que
 import sync.client.SynchronizationService as sc
+
 import pathlib
 import queue
-
-server_address = ('127.0.0.1', 50123)
-storage_root = pathlib.Path('client_storage')
-storage_root.mkdir(exist_ok=True, parents=True)
-stage_queue_view1 = que.SqliteQueue('client.db')
-stage_queue_view2 = que.SqliteQueue('client.db')
-stage_queue_view3 = que.SqliteQueue('client.db')
-cache = que.SqliteQueue('client.cache')
-sync_queue = queue.Queue()
+import argparse
+import logging
 
 
-sender_service = sender.SenderService(address=server_address, storage_root=storage_root,
-                                      stage_queue=stage_queue_view1, sync_queue=None)
+logger = logging.getLogger(__name__)
 
-sync_service = sc.SynchronizationService(sync_queue=sync_queue, queue_view=stage_queue_view2, storage_root=storage_root)
 
-labview_address = ('127.0.0.1', 50321)
-receiver_service = receiver.ReceiverService(labview_address, stage_queue_view3, storage_root, cache, sync_queue)
+def main():
+    logger.info('Setting up client')
+    config = {
+        'server': {
+            'host': '127.0.0.1',
+            'port': 50123
+        },
+        'provider': {
+            'host': '127.0.0.1',
+            'port': 50321,
+            'extensions': ['lvm', 'avi'],
+            'retry_time': 30,
+            'separator': '\n'
+        },
+        'storage_root': 'client_storage',
+
+        'stage_queue_path': 'state_storage/stage.queue',
+        'cache_path': 'state_storage/client.cache'
+    }
+
+
+    storage_root = pathlib.Path(config['storage_root'])
+    if storage_root.parent.exists() and not storage_root.parent.is_dir():
+        logger.error('')
+    storage_root.mkdir(exist_ok=True, parents=True)
+
+    stage_queue_path = config['stage_queue_path']
+    stage_queue_view1 = que.SqliteQueue(stage_queue_path)
+    stage_queue_view2 = que.SqliteQueue(stage_queue_path)
+    stage_queue_view3 = que.SqliteQueue(stage_queue_path)
+
+    cache = que.SqliteQueue(config['cache_path'])
+
+    sync_queue = queue.Queue()
+    logger.info('Starting synchronization service')
+    sync_service = sc.SynchronizationService(sync_queue=sync_queue, queue_view=stage_queue_view2,
+                                             storage_root=storage_root)
+
+    labview_address = (config['provider']['host'], int(config['provider']['port']))
+    logger.info('Starting receiver service')
+    receiver_service = receiver.ReceiverService(labview_address, stage_queue_view3, storage_root,
+                                                cache, sync_queue, extensions=config['provider']['extensions'],
+                                                separator=config['provider']['separator'])
+
+    server_address = (config['server']['host'], int(config['server']['port']))
+    logger.info('Starting sender service')
+    sender_service = sender.SenderService(address=server_address, storage_root=storage_root,
+                                          stage_queue=stage_queue_view1, sync_queue=None)
+
+
+main()
--- implementacja2/server.py
+++ implementacja2/server.py
@@ -1,11 +1,38 @@
 import sync.server.DataReceiverService as srv
 import sync.utils.PersistentQueue as que
+import pathlib as pl
+import logging
 
-address = ('127.0.0.1', 50123)
-cache = que.SqliteQueue('server.cache')
-storage_root = 'server_received'
+logger = logging.getLogger(__name__)
 
-server = srv.DataReceiverServer(address=address, cache=cache, handler_cls=srv.DataReceiverHandler,
-                                storage_root=storage_root)
 
-server.serve_forever()
+def main():
+    logger.info('Setting up server')
+    config = {
+        'host': '127.0.0.1',
+        'port': 50123,
+        'cache_path': 'state_storage/server.cache',
+        'storage_root': 'state_storage/server_storage_root'
+    }
+    address = config['host'], int(config['port'])
+    cache_path = pl.Path(config['cache_path'])
+    if cache_path.parent.exists() and not cache_path.parent.is_dir():
+        logger.error('Cache file parent directory is not a directory (you can use nonexistent dir instead)')
+        exit(-1)
+    cache_path.parent.mkdir(exist_ok=True, parents=True)
+    cache = que.SqliteQueue(str(cache_path))
+
+    storage_root = pl.Path(config['storage_root'])
+    if storage_root.parent.exists() and not storage_root.parent.is_dir():
+        logger.error('Storage root dir has to be nested under directory (current parent is not a directory)')
+        exit(-1)
+    storage_root.parent.mkdir(exist_ok=True, parents=True)
+
+    server = srv.DataReceiverServer(address=address, cache=cache, handler_cls=srv.DataReceiverHandler,
+                                    storage_root=storage_root)
+
+    logger.info('Listening {}'.format(address))
+    server.serve_forever()
+
+
+main()
