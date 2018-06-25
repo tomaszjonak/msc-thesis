4abcb213f7f00914f515fc006757337eff4222ec
Tomasz Jonak <jonak.t.o@gmail.com>
2018-06-25 23:55:14 +0200

    [implementacja] konfiguracje testowe oraz skrypt do monitorowania

--- implementacja/measure.py
+++ implementacja/measure.py
@@ -1,11 +1,12 @@
 import sqlite3
 import time
 import csv
+import sys
 
 
 def main():
     while True:
-        with sqlite3.connect("state_storage/stage.queue") as con, open("state_storage/queue.log", 'a') as fd:
+        with sqlite3.connect(sys.argv[1]) as con, open(sys.argv[2], 'a') as fd:
             ((count,),) = con.execute("SELECT count(*) FROM queue").fetchall()
             writer = csv.writer(fd, delimiter=',')
             writer.writerow([time.strftime("%Y/%m/%d %H:%M:%S"), count])
