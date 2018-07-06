dd5d50a139d008104c08e47ada531bdd4c10d192
Tomasz Jonak <jonak.t.o@gmail.com>
2018-07-06 12:18:10 +0200

    [pomiary] ostatnie poprawki

--- testy/monitorowanie_kolejki/plot_queue_log.py
+++ testy/monitorowanie_kolejki/plot_queue_log.py
@@ -25,14 +25,18 @@ def main():
     data.index.name = "Czas pomiaru"
 
     matplotlib.style.use('ggplot')
+    matplotlib.rcParams.update({'font.size': 18})
 
     fig, ax = plt.subplots()
 
-    data['Ilosc plikow do wyslania'].plot(ax=ax)
+    data['Ilosc plikow do wyslania'].iloc[::6].plot(ax=ax)
     # ax.yaxis.lim(ymin=0)
-    ax.set_title("Dlugosc kolejki plikow do wyslania")
+    ax.set_title("Wielkosc kolejki plikow do wyslania w czasie dzialania aplikacji")
+    ax.set_xlabel("Godzina")
+    ax.set_ylabel("Ilosc plikow oczekujacych na wyslanie")
     ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
-    ax.xaxis.set_minor_locator(mdates.HourLocator(interval=1))
+    ax.xaxis.set_minor_locator(mdates.HourLocator(byhour=range(0, 24, 2)))
+    # ax.xaxis.set_minor_locator(mdates.HourLocator(interval=1))
     ax.xaxis.set_major_formatter(mdates.DateFormatter('\n%d/%m'))
     ax.xaxis.set_minor_formatter(mdates.DateFormatter('%H'))
     ax.xaxis.grid(b=True, which='major', linewidth=2)
@@ -42,11 +46,11 @@ def main():
     fig, ax = plt.subplots()
 
     (data[2]/1000).plot(ax=ax)
-    # ax.yaxis.lim(ymin=0)
     ax.set_title("Wielkosc pliku kolejki")
-    # ax.xaxis.label = "Wielkosc pliku kolejki w kilobajtach"
+    ax.set_ylabel("Wielkosc pliku kolejki w kilobajtach")
+    ax.set_xlabel("Godzina")
     ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
-    ax.xaxis.set_minor_locator(mdates.HourLocator(interval=1))
+    ax.xaxis.set_minor_locator(mdates.HourLocator(byhour=range(0, 24, 2)))
     ax.xaxis.set_major_formatter(mdates.DateFormatter('\n%d/%m'))
     ax.xaxis.set_minor_formatter(mdates.DateFormatter('%H'))
     ax.xaxis.grid(b=True, which='major', linewidth=2)
